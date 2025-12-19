cf53a08549d8ee3e1016b56d7de044bef6ef0e86
achyuth706 <achyuthariga@gmail.com>
2025-12-20 00:03:48 +0530

    Added teacher portal dashboard with class roster and CSV export

---
+++ src/app/school/teacher/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import TeacherClassDashboard from '../../components/sections/TeacherClassDashboard';
+
+
+export default function TeacherPage() {
+  return <TeacherClassDashboard />;
+}
